e5833d49ffde0706ade6eaa7424fa1fba9a86425
Milnex <itsmings@gmail.com>
2012-04-26 23:36:04 +0800

    Getting NZEC, unknown bugs

--- 08847_FEELUCK/FEELUCK.py
+++ 08847_FEELUCK/FEELUCK.py
@@ -1,1 +1,9 @@
+from collections import defaultdict
+import re
 
+for case in range(input()):
+    URLs = defaultdict(list)
+    for count in range(10):
+        url, relevance = re.split(r'(\S{1,100})\s*(\d+)', raw_input())[1:3]
+        URLs[int(relevance)].append(url)
+    print 'Case #{0}:\n{1}'.format(case+1, '\n'.join(URLs[sorted(URLs)[-1]]))
