c573b38cc885d89cd172afea6c5d281cbe9a614b
Milnex <itsmings@gmail.com>
2012-04-27 14:54:06 +0800

    Add solution to 03638, be careful to whitespaces after the first line

--- 03638_WORDCNT/WORDCNT.py
+++ 03638_WORDCNT/WORDCNT.py
@@ -1,3 +1,10 @@
-for n in xrange(input()):
-    P, maxlen = 0, 0
-    for w in raw_input().split():
+from sys import stdin   
+
+stdin.readline()
+for string in stdin:
+    P, seqlen, wordlen = 0, 1, -1
+    for w in string.split():
+        curlen = len(w)
+        wordlen, seqlen = curlen, seqlen+1 if curlen == wordlen else 1
+        P = seqlen if seqlen > P else P
+    print P
